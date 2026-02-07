aeff73c8b0e6ac94969b146ab62db29fa4aa3b3d
Uomocosa <maggiori.samuele@gmail.com>
2026-02-07 09:43:06 +0100

    test non-circular imports

--- lele/__init__.py
+++ lele/__init__.py
@@ -3,18 +3,10 @@ new_import_system.install(__file__)
 
 # Define what happens when someone accesses an attribute of lele
 def __getattr__(name):
-    if name == "P":
-        from . import Path
-        return Path.P
-    
-    if name == "isinstance":
-        from . import Metaprogramming
-        return Metaprogramming.isinstance
-        
-    if name == "type":
-        from . import Metaprogramming
-        return Metaprogramming.get_type_from_lazy_module
-
+    import lele
+    if name == "P": return lele.Path.P
+    if name == "isinstance": return lele.isinstance
+    if name == "type": return lele.type
     raise AttributeError(f"module {__name__} has no attribute {name}")
 
 # This tells IDEs/Static Analyzers that these variables exist
