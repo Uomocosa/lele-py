04fd498ceabbaebb705acfb21702139e0a3c93d1
Uomocosa <maggiori.samuele@gmail.com>
2026-02-07 09:53:31 +0100

    test non-circular imports

--- lele/__init__.py
+++ lele/__init__.py
@@ -1,9 +1,9 @@
 import new_import_system
 new_import_system.install(__file__)
 
-from .Path import P
-from .Metaprogramming import get_type_from_lazy_module as type
-from .Metaprogramming import isinstance
+# from .Path import P
+# from .Metaprogramming import get_type_from_lazy_module as type
+# from .Metaprogramming import isinstance
 
-def test_():
-    print(P("."))
+# def test_():
+#     print(P("."))
