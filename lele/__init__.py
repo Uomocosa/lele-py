095d12cd1b1418c0016207498fe2051f196e31e7
Uomocosa <maggiori.samuele@gmail.com>
2026-02-07 08:56:55 +0100

    test non-circular imports

--- lele/__init__.py
+++ lele/__init__.py
@@ -2,7 +2,9 @@ import new_import_system
 new_import_system.install(__file__)
 
 # Simpler exports/imports:
+from . import Path as _Path
+from . import Metaprogramming as _Metaprogramming
 import lele
-P = lele.Path.P # for example you can call it via lele.P instead of lele.Path.P
-isinstance = lele.Metaprogramming.isinstance
-type = lele.Metaprogramming.get_type_from_lazy_module
+P = _Path.P # for example you can call it via lele.P instead of lele.Path.P
+isinstance = _Metaprogramming.isinstance
+type = _Metaprogramming.get_type_from_lazy_module
