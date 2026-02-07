1d91798e7b0c883b562c8ff9f5a08fea11d0ebdd
Uomocosa <maggiori.samuele@gmail.com>
2026-02-07 09:43:59 +0100

    test non-circular imports

--- lele/__init__.py
+++ lele/__init__.py
@@ -4,6 +4,9 @@ new_import_system.install(__file__)
 # Define what happens when someone accesses an attribute of lele
 def __getattr__(name):
     import lele
+    print(lele)
+    print(dir(lele))
+    print(lele.__file__)
     if name == "P": return lele.Path.P
     if name == "isinstance": return lele.isinstance
     if name == "type": return lele.type
